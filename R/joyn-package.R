1c3c0c638d38706f32874db40bae5c30eb455eb9
R.Andres Castaneda Aguilar <acastanedaa@worldbank.org>
2023-11-27 16:29:38 -0500

    create package doc

---
+++ R/joyn-package.R
@@ -0,0 +1,26 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+# Prevent R CMD check from complaining about the use of pipe expressions
+# standard data.table variables
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(
+    names = c(
+      ".",
+      ".I",
+      ".N",
+      ".SD",
+      ".",
+      "!!",
+      ":=",
+      "..output",
+      "x_report",
+      "y_report"
+    ),
+    package = utils::packageName()
+  )
+}
+
+NULL
