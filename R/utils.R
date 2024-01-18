f44491fdfa10e0717c29f846e0bf5eea7cd050dd
zander-prinsloo <prinsloo.zander@gmail.com>
2024-01-18 13:33:04 -0500

    is_balanced util function

--- R/utils.R
+++ R/utils.R
@@ -83,3 +83,90 @@ split_match_type <- function(match_type) {
     unlist()
 
 }
+
+
+#' Is data frame balanced by group?
+#'
+#'
+#'
+#' @param df data frame
+#' @param by  character: variables to check balance
+#' @param return character: either "logic" or "table". If logic, returns `TRUE`
+#'   or `FALSE` if data frame is balanced. If "table" returns the unbalanced
+#'   observations - i.e. the combinations not found in input `df`
+#'
+#' @return logical if return == "logic", else returns data frame of unbalanced observations
+#' @export
+#'
+#' @examples
+#' x1 = data.frame(id = c(1L, 1L, 2L, 3L, NA_integer_),
+#'                 t  = c(1L, 2L, 1L, 2L, NA_integer_),
+#'                 x  = 11:15)
+#' is_balanced(df = x1,
+#'             by = c("id", "t"),
+#'             return = "table") # returns combo of "id" and "t" not in df
+#' is_balanced(df = x1,
+#'             by = c("id", "t"),
+#'             return = "logic") # returns TRUE or FALSE
+is_balanced <- function(df,
+                        by,
+                        return = c("logic", "table")) {
+
+  #_____________________________________________________
+  # Arguments-------------------------------------------
+  return <- match.arg(return)
+  if (!is.character(by)) {
+    cli::cli_abort("Argument `by` must be character vector")
+  }
+  if (!all(by %in% names(df))) {
+    cli::cli_abort("`by` must give column names of `df`")
+
+  }
+  #_____________________________________________________
+  # unique values of "by" ------------------------------
+  lt_base <- lapply(by,
+                    function(y){
+                      df |>
+                        get_vars(y) |>
+                        collapse::funique()
+                    })
+
+  #_____________________________________________________
+  # name of list elements ------------------------------
+  names(lt_base) <- by
+
+  #_____________________________________________________
+  # elements as vectors, removing NAs ------------------
+  lt_base <- lapply(lt_base,
+                        function(df) {
+                          df[[1]] |>
+                            na_omit()
+                        })
+
+  #_____________________________________________________
+  # expand grid ----------------------------------------
+  df_balanced <-
+    expand.grid(lt_base,
+                stringsAsFactors = FALSE) |>
+    collapse::qDF()
+
+  #_____________________________________________________
+  #
+  #unmatch  <- dt_balanced[!dt,  on = by]
+  unmatch   <- collapse::join(
+    x       = df_balanced,
+    y       = df,
+    how     = "anti",
+    verbose = FALSE
+  )
+  balanced <- c(nrow(unmatch) == 0)
+
+  if (return == "logic") {
+    return(balanced)
+  } else {
+    setattr(unmatch, "is_balanced", balanced)
+    return(unmatch)
+  }
+
+}
+
