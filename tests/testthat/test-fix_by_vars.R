7885bd8b845cef256d6b4d46cc5e77422254f8b8
RossanaTat <rossana.tatulli@studbocconi.it>
2024-01-25 18:30:01 -0500

    test fix_by_vars

---
+++ tests/testthat/test-fix_by_vars.R
@@ -0,0 +1,12 @@
+# Test function fixing variable names in by argument
+
+test_that("fix_by_vars -ouput", {
+  res <- fix_by_vars(by ="id", x1, y1)
+
+  class(res) |>
+    expect_equal("list")
+  
+  names(res) |>
+    expect_equal(c("by", "xby", "yby", "tempkey"))
+})
+
