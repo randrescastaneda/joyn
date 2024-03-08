04a3f7ba1985d20ee43e3d9967a77213f87a6835
RossanaTat <rossana.tatulli@studbocconi.it>
2024-03-08 13:29:21 -0500

    get_joyn_options v0

--- R/zzz.R
+++ R/zzz.R
@@ -9,7 +9,32 @@
     joyn.na.last         = FALSE
   )
   toset <- !(names(op.joyn) %in% names(op))
-  if(any(toset)) options(op.joyn[toset])
+
+  #store it in .joynenv
+  rlang::env_bind(.joynenv, op.joyn = op.joyn)
+
+  if(any(toset)) {
+    options(op.joyn[toset])
+  }
+
+  #get_joyn_options()
 
   invisible()
+}
+
+get_joyn_options <- function() {
+
+  # Show default values of all possible options
+  defaults <- lapply(names(op.joyn), function(opt) {
+    list(option = opt, default_value = op.joyn[[opt]])
+  })
+
+  # Show current values of all possible options
+  current_values <- lapply(names(op.joyn), function(opt) {
+    list(option = opt, current_values = getOption(opt))
+  })
+
+  # See and print
+  cat("Joyn Options:\n")
+  cat(do.call(sprintf, c("%s: Default=%s, Current=%s\n", defaults, current_values)))
 }
