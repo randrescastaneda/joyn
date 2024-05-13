5f56d925b85f5b6803a22230d4d254dec1c91a8f
zander-prinsloo <prinsloo.zander@gmail.com>
2024-05-13 16:00:14 -0400

    test changing of input data frames

--- tests/testthat/test-dplyr-joins.R
+++ tests/testthat/test-dplyr-joins.R
@@ -2047,3 +2047,61 @@ test_that("ANTI JOIN - NA matches", {
 })
 
 
+
+
+
+test_that("joyn() - input data unchanged", {
+
+  expect_equal(x1,
+               data.table(id = c(1L, 1L, 2L, 3L, NA_integer_),
+                          t  = c(1L, 2L, 1L, 2L, NA_integer_),
+                          x  = 11:15))
+
+  expect_equal(y1,
+               data.table(id = c(1,2, 4),
+                          y  = c(11L, 15L, 16)))
+
+  expect_equal(x2,
+               data.table(id = c(1, 4, 2, 3, NA),
+                          t  = c(1L, 2L, 1L, 2L, NA_integer_),
+                          x  = c(16, 12, NA, NA, 15)))
+
+  expect_equal(y2,
+               data.table(id = c(1, 2, 5, 6, 3),
+                          yd = c(1, 2, 5, 6, 3),
+                          y  = c(11L, 15L, 20L, 13L, 10L),
+                          x  = c(16:20)))
+
+  expect_equal(x3,
+               data.table(id  = c("c","b", "d"),
+                          v   = 8:10,
+                          foo = c(4,2, 7)))
+
+  expect_equal(y3,
+               data.table(id = c("c","b", "c", "a"),
+                          y  = c(11L, 15L, 18L, 20L)))
+
+  expect_equal(x4,
+               data.table(id1 = c(1, 1, 2, 3, 3),
+                          id2 = c(1, 1, 2, 3, 4),
+                          t   = c(1L, 2L, 1L, 2L, NA_integer_),
+                          x   = c(16, 12, NA, NA, 15)))
+
+  expect_equal(y4,
+               data.table(id  = c(1, 2, 5, 6, 3),
+                          id2 = c(1, 1, 2, 3, 4),
+                          y   = c(11L, 15L, 20L, 13L, 10L),
+                          x   = c(16:20)))
+
+  # changed
+  expect_equal(x5,
+               data.table(id = c(1L, 1L, 2L, 3L, NA_integer_, NA_integer_),
+                          t  = c(1L, 2L, 1L, 2L, NA_integer_, 4L),
+                          x  = 11:16))
+
+  expect_equal(y5,
+               data.table(id = c(1,2, 4, NA_integer_, NA_integer_),
+                          y  = c(11L, 15L, 16, 17L, 18L)))
+
+})
+
