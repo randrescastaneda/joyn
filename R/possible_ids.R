1bd7aee57eae8e122e98248885ec8d840ffbfa21
R.Andres Castaneda Aguilar <acastanedaa@worldbank.org>
2024-09-20 10:30:38 -0400

    update

--- R/possible_ids.R
+++ R/possible_ids.R
@@ -26,8 +26,10 @@ possible_ids <- function(dt,
                          include = NULL,
                          exclude_types = NULL,
                          include_types = NULL,
-                         verbose = getOption("possible_ids.verbose", default = FALSE),
-                         comb_size = 5,
+                         verbose = getOption("possible_ids.verbose",
+                                             default = FALSE),
+                         max_combination_size = 5,
+                         max_processing_time = 60, # in seconds
                          get_all  = FALSE) {
 
   # defenses ---------
@@ -54,54 +56,70 @@ possible_ids <- function(dt,
   vars <- filter_by_name(vars, include, exclude)
 
 
-
-
-
-
-
   # Remove duplicate column names... just in case
   vars <- unique(vars)
 
   if (length(vars) == 0) {
     if (verbose) {
-      cli::cli_alert_danger("No variables available after applying include/exclude filters.")
+      cli::cli_alert_danger("No variables available after applying
+                            include/exclude filters.")
     }
     return(NULL) # should this be an error?
   }
 
-  # Check if any variables uniquely identify the data individually
-  unique_vars <- vapply(vars,
-                        \(var) !anyDuplicated(dt[[var]]),
-                        logical(1))
+  # Unique values ---------
 
-  # Collect variables that are unique identifiers
-  unique_ids <- vars[unique_vars]
+  # Sort variables by number of unique values (ascending order)
+  unique_counts <- vapply(dt[, ..vars], fnunique, numeric(1))
+  vars          <- vars[order(unique_counts)]
+  unique_counts <- unique_counts[order(unique_counts)]
+  n_row         <- fnrow(dt)
+  unique_ids    <- vars[unique_counts == n_row]
 
   # Initialize list to store possible IDs
   possible_ids_list <- list()
-
   # Add individual unique variables
   if (length(unique_ids) > 0) {
     possible_ids_list <- c(possible_ids_list, as.list(unique_ids))
     if (verbose) {
       cli::cli_alert_info("Found unique identifiers: {.code {unique_ids}}")
     }
+    if (!get_all) return(possible_ids_list)
   }
 
   # Remove unique identifiers from vars to reduce combinations
   vars <- setdiff(vars, unique_ids)
-
-  # If data is uniquely identified by existing variables, return the unique IDs
-  if (length(possible_ids_list) > 0 && fnrow(dt) == fnrow(unique(dt[, ..unique_ids]))) {
+  if (length(vars) == 0) {
+    # All variables are unique identifiers
     return(possible_ids_list)
   }
+  unique_counts <- unique_counts[vars]
 
-  # Start with combinations of size 2 up to the number of remaining vars
-  for (i in 2:min(length(vars), comb_size)) {  # Limit combination size to 5 for efficiency
-    combos <- combn(vars, i, simplify = FALSE)
+  # combinations -----------
+
+  # Start testing combinations
+  start_time <- Sys.time()
+  max_size <- min(length(vars), max_combination_size)
+
+  for (comb_size in 2:max_size) {
+
+    combos <- combn(vars, comb_size, simplify = FALSE)
+
+    # Prune combinations where the product of unique counts is less
+    # than n_rows
+    combos_to_keep <- vapply(combos, \(combo) {
+      prod(unique_counts[combo]) >= n_rows
+    }, logical(1))
+
+    combos <- combos[combo_to_keep]
+
+    # Estimate processing time and prune combinations
+    est_times <- vapply(combos, function(combo) {
+      estimate_combination_time(n_rows, unique_counts[combo])
+    }, numeric(1))
 
     if (verbose) {
-      msg <- sprintf("combinations of %s variables", i)
+      msg <- sprintf("combinations of %s variables", comb_size)
       cli::cli_progress_bar(msg, total = length(combos))
     }
 
@@ -114,14 +132,29 @@ possible_ids <- function(dt,
         if (!get_all) {
           return(possible_ids_list)
         }
-        # Remove variables in the current combo from vars to avoid redundant checks
+        # Remove variables in the current combo from vars to
+        # avoid redundant checks
         vars <- setdiff(vars, combo)
+        unique_counts <- unique_counts[vars]
         break  # Break since we found a minimal unique key of size i
+      }
+      # Check processing time
+      elapsed_time <- as.numeric(difftime(Sys.time(),
+                                          start_time,
+                                          units = "secs"))
+      if (elapsed_time > max_processing_time) {
+        if (verbose) {
+          mxt_msg <- "Maximum processing time exceeded.
+          modify {.arg max_processing_time} argument to increse time.
+          Stopping search."
+          cli::cli_alert_warning(mxt_msg)
+        }
+        break
       }
       if (verbose) cli::cli_progress_update()
     }
     # Break if all variables are used
-    if (length(vars) == 0) {
+    if (length(vars) == 0 || elapsed_time > max_processing_time) {
       break
     }
   }
@@ -165,4 +198,15 @@ filter_by_name <- function(vars, include, exclude) {
     vars <- setdiff(vars, exclude)
   }
   vars
+}
+
+
+
+# Function to estimate processing time based on unique counts
+estimate_combination_time <- function(n_rows, unique_counts) {
+  # Simple estimation function
+  # Time is proportional to (product of unique counts) / n_rows
+  # Adjust the constant factor based on empirical observations
+  est_time <- (prod(unique_counts) / n_rows) * 0.0001
+  return(est_time)
 }
