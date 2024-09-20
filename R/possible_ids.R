209f272cd16259852e0b2540d11618efa7bc37cc
R.Andres Castaneda Aguilar <acastanedaa@worldbank.org>
2024-09-20 12:51:01 -0400

    update format

--- R/possible_ids.R
+++ R/possible_ids.R
@@ -28,8 +28,10 @@ possible_ids <- function(dt,
                          include_types = NULL,
                          verbose = getOption("possible_ids.verbose",
                                              default = FALSE),
+                         min_combination_size = 1,
                          max_combination_size = 5,
                          max_processing_time = 60, # in seconds
+                         max_numb_possible_ids = 100,
                          get_all  = FALSE) {
 
   # defenses ---------
@@ -59,6 +61,10 @@ possible_ids <- function(dt,
   # Remove duplicate column names... just in case
   vars <- unique(vars)
 
+  if (verbose) {
+    cli::cli_alert_info("Variables to test: {.field {vars}}")
+  }
+
   if (length(vars) == 0) {
     if (verbose) {
       cli::cli_alert_danger("No variables available after applying
@@ -74,34 +80,43 @@ possible_ids <- function(dt,
   vars          <- vars[order(unique_counts)]
   unique_counts <- unique_counts[order(unique_counts)]
   n_row         <- fnrow(dt)
-  unique_ids    <- vars[unique_counts == n_row]
+  init_index    <- 0
+
+
 
   # Initialize list to store possible IDs
-  possible_ids_list <- list()
-  # Add individual unique variables
-  if (length(unique_ids) > 0) {
-    possible_ids_list <- c(possible_ids_list, as.list(unique_ids))
-    if (verbose) {
-      cli::cli_alert_info("Found unique identifiers: {.code {unique_ids}}")
-    }
-    if (!get_all) return(possible_ids_list)
-  }
+  possible_ids_list <- vector("list", max_numb_possible_ids)
 
-  # Remove unique identifiers from vars to reduce combinations
-  vars <- setdiff(vars, unique_ids)
-  if (length(vars) == 0) {
-    # All variables are unique identifiers
-    return(possible_ids_list)
+  if (min_combination_size == 1) {
+    unique_ids    <- vars[unique_counts == n_row]
+    # Add individual unique variables
+    init_index <- length(unique_ids)
+    if (init_index > 0) {
+      possible_ids_list[1:init_index] <- as.list(unique_ids)
+      if (verbose) {
+        cli::cli_alert_info("Found unique identifiers: {.code {unique_ids}}")
+      }
+      if (!get_all) return(remove_null(possible_ids_list))
+
+      # Remove unique identifiers from vars to reduce combinations
+      vars <- setdiff(vars, unique_ids)
+      if (length(vars) == 0) {
+        # All variables are unique identifiers
+        return(remove_null(possible_ids_list))
+      }
+      unique_counts <- unique_counts[vars]
+    }
   }
-  unique_counts <- unique_counts[vars]
 
   # combinations -----------
 
   # Start testing combinations
   start_time <- Sys.time()
   max_size <- min(length(vars), max_combination_size)
+  min_size <- max(min_combination_size, 2)
 
-  for (comb_size in 2:max_size) {
+  j <- init_index + 1
+  for (comb_size in min_size:max_size) {
 
     combos <- combn(vars, comb_size, simplify = FALSE)
 
@@ -111,7 +126,7 @@ possible_ids <- function(dt,
       prod(unique_counts[combo]) >= n_rows
     }, logical(1))
 
-    combos <- combos[combo_to_keep]
+    combos <- combos[combos_to_keep]
 
     # Estimate processing time and prune combinations
     est_times <- vapply(combos, function(combo) {
@@ -119,8 +134,10 @@ possible_ids <- function(dt,
     }, numeric(1))
 
     if (verbose) {
-      msg <- sprintf("combinations of %s variables", comb_size)
-      cli::cli_progress_bar(msg, total = length(combos))
+      cli::cli_progress_bar(
+        format = "combs of {cli::pb_extra$comb_size} vars: {cli::pb_bar} {cli::pb_percent} | ETA: {cli::pb_eta} | {cli::pb_current}/{cli::pb_total}",
+        extra = list(comb_size = comb_size),
+        total = length(combos))
     }
 
     for (combo in combos) {
@@ -128,15 +145,26 @@ possible_ids <- function(dt,
       if (is_id(dt, by = combo, verbose = FALSE)) {
         # This is inefficient... it is copying every time...
         # I need to think better on how to do it.
-        possible_ids_list <- c(possible_ids_list, list(combo))
-        if (!get_all) {
+        possible_ids_list[j] <- combo
+        j <- init_index + 1
+        if (j > max_numb_possible_ids) {
+          if (verbose) {
+            cli::cli_alert_warning(
+            "Max number of possible IDs ({max_numb_possible_ids}) reached.
+            You may modify it in argument {.arg max_numb_possible_ids}")
+          }
           return(possible_ids_list)
+        }
+        if (!get_all) {
+          return(remove_null(possible_ids_list))
         }
         # Remove variables in the current combo from vars to
         # avoid redundant checks
         vars <- setdiff(vars, combo)
         unique_counts <- unique_counts[vars]
-        break  # Break since we found a minimal unique key of size i
+
+        # Break since we found a minimal unique key of size i
+        if (!get_all) break
       }
       # Check processing time
       elapsed_time <- as.numeric(difftime(Sys.time(),
@@ -159,13 +187,13 @@ possible_ids <- function(dt,
     }
   }
 
-  if (length(possible_ids_list) == 0) {
+  if (length(remove_null(possible_ids_list)) == 0) {
     if (verbose) {
       cli::cli_alert_warning("No unique identifier found.")
     }
     return(NULL)
   } else {
-    return(possible_ids_list)
+    return(remove_null(possible_ids_list))
   }
 }
 
@@ -188,16 +216,14 @@ filter_by_class <- function(dt, vars, include_types, exclude_types) {
 }
 
 filter_by_name <- function(vars, include, exclude) {
-  # Apply 'include' filter
-  if (!is.null(include)) {
-    vars <- intersect(vars, include)
-  }
-
   # Apply 'exclude' filter
   if (!is.null(exclude)) {
     vars <- setdiff(vars, exclude)
   }
-  vars
+
+  # Apply 'include' filter
+  c(vars, include)
+
 }
 
 
@@ -209,4 +235,10 @@ estimate_combination_time <- function(n_rows, unique_counts) {
   # Adjust the constant factor based on empirical observations
   est_time <- (prod(unique_counts) / n_rows) * 0.0001
   return(est_time)
+}
+
+
+remove_null <- \(x) {
+  y <- vapply(x, \(.) !is.null(.), logical(1))
+  x[y]
 }
