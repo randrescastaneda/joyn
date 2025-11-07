ee08181c434180705399025afb823bcec944ff9d
RossanaTat <rossana.tatulli@studbocconi.it>
2025-11-07 15:47:52 -0500

    clean code

--- R/fix_by_vars.R
+++ R/fix_by_vars.R
@@ -6,38 +6,6 @@
 #'
 #' @return list
 #' @noRd
-# fix_by_vars <- function(by, x, y) {
-#
-#   byexp <- grep("==?", by, value = TRUE)
-#
-#   if (length(byexp) != 0) {
-#
-#     xby <- trimws(gsub("([^=]+)(\\s*==?\\s*)([^=]+)", "\\1", byexp))
-#     yby <- trimws(gsub("([^=]+)(\\s*==?\\s*)([^=]+)", "\\3", byexp))
-#     newkeys <- paste0("keyby", 1:length(xby))
-#
-#     setnames(x, xby, newkeys)
-#     setnames(y, yby, newkeys)
-#
-#     by[grepl("==?", by)] <- newkeys
-#
-#     return(list(by      = by,
-#                 xby     = xby,
-#                 yby     = yby,
-#                 tempkey = newkeys)
-#            )
-#
-#   } else {
-#
-#     return(list(by      = by,
-#                 xby     = NULL,
-#                 yby     = NULL,
-#                 tempkey = NULL)
-#            )
-#
-#   }
-#
-# }
 fix_by_vars <- function(by, x, y) {
 
   byexp <- grep("==?", by, value = TRUE)
